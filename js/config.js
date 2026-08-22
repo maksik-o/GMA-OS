aeef7ae12c9b235cf3c5523369f2feff3e5c1dc1
maksik-o <maksimgomzyakov1@gmail.com>
2026-08-23 01:31:32 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // Настройки синхронизации. Заполняются один раз после развертывания Apps Script.
-export const API_URL = 'https://script.google.com/macros/s/AKfycbyHJFiSGKDxjovcWiECFNw7gECrpomYq7vxhEiJd9nUKc_x4alippHJgEOmw3O5HxHGLQ/exec';                            // заканчивается на /exec
+export const API_URL = 'https://script.google.com/macros/s/AKfycbykYiXRrcFkIV4jC6g-0Tzruui_vcjSrBb5WkDLAEYFyAg0gM5_SHm3Y5Ibz86lziUu3w/exec';                            // заканчивается на /exec
 export const CLIENT_ID = '40124617550-80jhol56dkg6jq84l14k96d57kvrhret.apps.googleusercontent.com'; // из console.cloud.google.com
